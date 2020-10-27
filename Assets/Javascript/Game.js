f471fb52d9c105fff2cd0b74e9bc3659db2a52b6
sahilkashyap78 <sahilkashyap78@gmail.com>
2020-10-27 17:56:22 +0530

    basic code structure for game.js

---
+++ Assets/Javascript/Game.js
@@ -0,0 +1,29 @@
+function Game()
+{
+	
+	this.init = initializer;
+	
+	function initializer()
+	{
+        
+            
+	}
+	
+	function gameLoop()
+	{
+		update();
+		draw();
+	}
+	
+	function update()
+	{
+		
+	}
+	
+	function draw()
+	{
+		
+	}
+	
+	
+}
