6d1c6dfe4363e954592d483bb8a46f5a5bdd3321
sahilkashyap78 <sahilkashyap78@gmail.com>
2020-10-27 19:44:29 +0530

    no message

--- Assets/Javascript/Game.js
+++ Assets/Javascript/Game.js
@@ -1,10 +1,13 @@
 function Game()
 {
+    var lasttime;
 	
 	this.init = initializer;
+    var speed = 100; //in pixel per second
 	
 	function initializer()
 	{
+        lastTime = new Date().getTime();
         setIntervalID = setInterval(gameLoop, 10);
         
             
@@ -18,10 +21,12 @@ function Game()
 	
 	function update()
 	{
-    
-        
-        road1PositionY += 4;
-	    road2PositionY += 4;
+        var currTime = new Date().getTime(); // Let's say the current time is 9:30:35:180
+		var timeElapsed = currTime - lastTime; // So, timeElapsed will be (180 - 156) 24 ms
+		lastTime = currTime;
+        var distance = (timeElapsed / 1000) * speed;
+        road1PositionY  += distance;
+	    road2PositionY += distance;
         if (road1PositionY >= gameHeight)
         {
             road1PositionY = -gameHeight;
