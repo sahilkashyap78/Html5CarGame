a45fb15cb3d5bb8cefad0ff257198d5e15e68ba6
sahilkashyap78 <sahilkashyap78@gmail.com>
2020-10-28 12:20:37 +0530

    constants added for car

--- Assets/Javascript/Constant.js
+++ Assets/Javascript/Constant.js
@@ -1,6 +1,11 @@
 var IMAGES_PATH = "Assets/Image/";
 var gameWidth = 500;
-var gameHeight = 600;
+var gameHeight = 590;
 var road1PositionY = 0;
 var road2PositionY = -gameHeight;
+var leftEdgeOfRoad = 140;
+var rightEdgeOfRoad = 350;
+var carPositionX = 200;
+var carPositionY = 450;
+
 
